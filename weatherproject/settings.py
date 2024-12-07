0c2f1357b7385d86eb4311cb6ea027317aa36f2e
Hariprasath2707 <hariprasath642@gmail.com>
2024-12-07 16:14:54 +0530

    fix

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -11,18 +11,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-# import os
+from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*yqd0+sg(9yjw5w%1xrsjshwpp0xnu^-ks9)&z@-*&fh*rzzqs'
+# SECRET_KEY = 'django-insecure-*yqd0+sg(9yjw5w%1xrsjshwpp0xnu^-ks9)&z@-*&fh*rzzqs'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-*yqd0+sg(9yjw5w%1xrsjshwpp0xnu^-ks9)&z@-*&fh*rzzqs')
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
